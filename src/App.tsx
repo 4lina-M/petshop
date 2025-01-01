36b27dc84e4ab45080f91d73e7898f0916191e60
4L1N4M <alina.montijoz@gmail.com>
2025-01-01 00:57:47 -0700

    Create Home Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
 // import { useState } from 'react'
 import './App.css'
 import { HiOutlineShoppingCart } from "react-icons/hi";
-import { MdLocalPhone } from "react-icons/md";
 import { MdOutlinePets } from "react-icons/md";
-
-
+import { ImSearch } from "react-icons/im";
 function Bar(){
   return (
     <><div style={
@@ -12,7 +10,7 @@ function Bar(){
         alignItems: "center",
         justifyContent: "center",
         display: "flex",
-        backgroundColor:"#465675",
+        backgroundColor:'#126e54',
         width: "100vw",
         color: "#ff8306",
         position: "fixed",
@@ -20,22 +18,50 @@ function Bar(){
         left:"0",
       }
     }>
+    {/* TODO: Convert paw into a home-page button */}
       <span className='classSpanPaw'><MdOutlinePets size="39px"/></span>
       <h1 className='classTitle'>PETS FATE</h1>
       <div style={
         {
-          width:"15%",
+          width:"10%",
           
         }
       }></div>
       <input className='classInputSearch' placeholder='Buscar artículos'></input>
+      <button className='classButtonSearch'><ImSearch size="19px"/></button>
       <button className='hoverButton'>Sobre nosotros</button>
       <button className='hoverButton'>Nueva mercancía</button>
       <button className='hoverButton'>Contacto</button>
-      <button className='classButtonCart'><HiOutlineShoppingCart size="25px"/></button>
+      <button className='classButtonCart'><HiOutlineShoppingCart size="35px"/></button>
       <button className='classButtonUser'>A</button>
   </div>
   </>
+  )
+}
+function Presentation(){
+  return(
+    <div className="homeImage" style={
+      {
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+        backgroundColor: "#dfe3ec",
+        color: "#ff8306",
+        width: "100vw",
+        height: "85vh",
+        marginTop: "100px",
+        backgroundAttachment: "fixed",
+      }
+    }>
+      <h1 className='classTitleHome'>PETS FATE</h1>
+      <div style={
+        {
+          height:"10%",
+        }
+      }></div>
+      <h2 className='classSubtitle'>Todo lo que tu mascota necesita</h2>
+    </div>
   )
 }
 
@@ -51,6 +77,7 @@ function App() {
         }
       }>
         <Bar></Bar>
+        <Presentation></Presentation>   
       </div>
     </>
   )
